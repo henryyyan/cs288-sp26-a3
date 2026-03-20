43e156540d7bdb286a2d276a3201f44c3719aa5a
Henry Yan <henryyan123@Henrys-MacBook-Pro.local>
2026-03-20 16:07:10 -0700

    Update generate.py and run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cat src/RAG/data_storage_100-60.json.part.* > src/RAG/data_storage_100-60.json
-cat src/RAG/eecs_ind_100-60.faiss.part.* > src/RAG/eecs_ind_100-60.faiss
+cat src/RAG/data_storage_100-20.json.part.* > src/RAG/data_storage_100-20.json
+cat src/RAG/eecs_ind_100-20.faiss.part.* > src/RAG/eecs_ind_100-20.faiss
 python3 src/RAG/generate.py "$1" "$2"
