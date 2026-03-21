d62d7765e1f76233109e1ed62c89e3e32fdde2d1
Henry Yan <henryyan123@Henrys-MacBook-Pro.local>
2026-03-20 18:56:41 -0700

    add 300-60 files and update RAG folder

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cat data_storage_100-60.json.part.* > data_storage_100-60.json
-cat eecs_ind_100-60.faiss.part.* > eecs_ind_100-60.faiss
+cat data_storage_300-60.json.part.* > data_storage_300-60.json
+cat eecs_ind_300-60.faiss.part.* > eecs_ind_300-60.faiss
 python3 src/RAG/generate.py "$1" "$2"
