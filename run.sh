dc3fac98b811d1dff75b92e5918d6faeb0e61f97
Henry Yan <henryyan123@Henrys-MacBook-Pro.local>
2026-03-20 18:38:22 -0700

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cat data_storage_100-20.json.part.* > data_storage_100-20.json
-cat eecs_ind_100-20.faiss.part.* > eecs_ind_100-20.faiss
+cat data_storage_100-60.json.part.* > data_storage_100-60.json
+cat eecs_ind_100-60.faiss.part.* > eecs_ind_100-60.faiss
 python3 src/RAG/generate.py "$1" "$2"
