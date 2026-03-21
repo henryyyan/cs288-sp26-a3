f35266d91925253dfffc2ed94693de620636ddad
Henry Yan <henryyan123@Henrys-MacBook-Pro.local>
2026-03-20 18:47:35 -0700

    add 100-20 files

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cat src/RAG/data_storage_100-60.json.part.* > src/RAG/data_storage_100-60.json
-cat src/RAG/eecs_ind_100-60.faiss.part.* > src/RAG/eecs_ind_100-60.faiss
+cat data_storage_100-20.json.part.* > data_storage_100-20.json
+cat eecs_ind_100-20.faiss.part.* > eecs_ind_100-20.faiss
 python3 src/RAG/generate.py "$1" "$2"
