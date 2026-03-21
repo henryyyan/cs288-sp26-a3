e96aeffcf31b953e261df087abba62eb8bcdaf7a
Henry Yan <henryyan123@Henrys-MacBook-Pro.local>
2026-03-20 18:57:26 -0700

    none

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cat data_storage_300-60.json.part.* > data_storage_300-60.json
-cat eecs_ind_300-60.faiss.part.* > eecs_ind_300-60.faiss
+cat data_storage_300-20.json.part.* > data_storage_300-20.json
+cat eecs_ind_300-20.faiss.part.* > eecs_ind_300-20.faiss
 python3 src/RAG/generate.py "$1" "$2"
--- src/RAG/generate.py
+++ src/RAG/generate.py
@@ -12,8 +12,8 @@ from pathlib import Path
 def load():
     script_dir = Path("/Users/henryyan123/Desktop/cs288-sp26-a3")
     
-    index_path = script_dir / "eecs_ind_300-60.faiss"
-    json_path = script_dir / "data_storage_300-60.json" 
+    index_path = script_dir / "eecs_ind_300-20.faiss"
+    json_path = script_dir / "data_storage_300-20.json" 
     df = pd.read_json(str(json_path), orient="records")
     
     model = SentenceTransformer('BAAI/bge-small-en-v1.5')
